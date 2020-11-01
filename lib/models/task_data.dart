951e762eaf7a8a6f9e54938b22c8f9ff142ea5ee
Eros Borba <erosborba@iMac.local>
2020-11-01 17:38:12 -0300

    add New ToDo List and Delete tasks function

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/cupertino.dart';
-
+import 'dart:collection';
 import 'package:flutter/foundation.dart';
 import 'package:todoey_flutter/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
-    Task(name: 'buy milk'),
-    Task(name: 'buy bread'),
-    Task(name: 'pay the bills'),
-  ];
+  List<Task> _tasks = [];
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
 
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
   }
 }
